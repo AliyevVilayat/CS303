315f72a7056fa9e2adf9c089253ec352507212fd
AliyevVilayat <77es4pa@code.edu.az>
2024-05-20 18:00:25 +0400

    Lesson 6 has been created

---
+++ Lesson 6/CS303-05172024/CS303-05172024/Program.cs
@@ -0,0 +1,33 @@
+﻿
+//variablelara mutleq deyer set olunmalidir yeni deyer assign olunmalidir başqa bir dildə desək dəyər mənimsədilməlidir. Əks təqdirdə biz onu nə ekrana çıxara bilmərik nə də başqa bir variabla mənimsədə bilmərik. Buradan anlıyırıq ki variablelara default dəyər avtomatik olaraq set olunmur
+
+
+
+
+using CS303_05172024;
+
+//className objectName = new className(); bu anlayış reference almaqdır, reference almaqdır və ya object newlamaqdır.
+//bərabərliyin sol tərəfi  reference hissə adlanır və stack yaddaşda saxlanılır.
+
+// Bərabərliyin sağ tərəfi isə bu referenceın reference etdiyi dəyəri yəni obyektin dəyəri və ya obyektin özü heap yaddaşda saxlanılacaq. Və nə zaman mən bu obyektə müraciət etmək istəsəm onun referencenı çağıracam və üzərində əməliyyatlar aparacam.
+
+Students studentMehemmed = new Students()
+{
+    name = "Mehemmed",
+    surname = "Baxışov",
+};
+
+Console.WriteLine(studentMehemmed.name);
+//studentMehemmed.name => bizim üçün get etməkdir. Yəni datanı oxumaq üçün ramdan çağırırıq.
+
+studentMehemmed.name = "Mehemmed";
+studentMehemmed.surname = "Baxışov";
+studentMehemmed.age = 20;
+studentMehemmed.email = "mehemmed.baxisov@code.edu.az";
+studentMehemmed.pinCode = "123ABCD";
+studentMehemmed.username = "mehemmedCrazyBoy";
+studentMehemmed.password = "parolusindirma";
+studentMehemmed.phoneNumber = "+994502111111";
+studentMehemmed.address = "Azərbaycan, Baku";
+
+Console.WriteLine($"{studentMehemmed.name} {studentMehemmed.surname} {studentMehemmed.age} ");
---
+++ Lesson 6/CS303-05172024/CS303-05172024/Students.cs
@@ -0,0 +1,63 @@
+﻿
+#region Notes
+
+//Snif deklarasiyasi : Access modifier + class keyword + className
+//Snif adları hər zaman böyük hərflə başlamalıdır, yəni Pascal case.
+
+//Access modifiers : public protected internal private
+//Access modifiers teyin etdiyimiz c# modulunun, yəni class, field  elcatanligini teyin edir.
+
+//ctrl + R + G : lazimsiz usingleri silmek ucun shorthand
+
+//Classların qarşısında heç bir Access Modifier keyword-ü yazılmazsa default olaraq Classlara internal access modifier teyin edilir, nested classlar yəni iç-içə classlar bu şamil edilmir. Çünki Nested classlar bizim classın üzvü yəni class memberları sayılır.
+
+//Class memberler: Field
+
+//Field və Property
+
+//Classın scopeları daxilində təyin olunmuş dəyişənlər yəni variable-lar FIELD adlanır.
+
+//Fieldlar həmçinin class memberlara daxildir.
+
+//Access modifierlar həmçinin Fieldlara da aid edilir.
+
+// Adlandırma zamanı Fieldlar declare olarkən adları kiçik hərflə başlayır, yəni adlandırma camel case olacaq.
+
+//Biz classın scopeları daxilində təyin etdiyimiz field və propertyləri declare etdiyimiz yerdə dəyər set etməzsək yəni dəyər mənimsədməzsək, avtomatik olaraq default dəyər onlara set edilir.
+
+//Biz daha əvvəldən görmüşdük ki variableları declare edən zaman onlara dəyər set olunmazsa özü avtomatik olaraq default dəyər set etmirdi. Fieldların adi variablelardan fərqi də budur. 2-ci fərqi isə class-a yəni bir type-a aid olmasıdır.
+
+//database-də mənim table-larıma qarşılıq c#-da entitylər yəni modeller hal-hazırda anlıya biləcəyimiz şəkildə desək class-larımız qarşılıq gəlir.
+
+//database-dəki table-ın daxilindəki columnlar bizim entitylərimizin yəni class-larımızın fieldlarına qarşılıq gəlir.
+
+//Constructor - class-dan instance alınan zaman yəni classdan object törənən zaman işə düşən bir funksiya. C# daxilində source codelardan Assembly səviyəsinə keçid edilən zaman hər bir class üçün arxa tərəfdə boş constructor təyin edilir.
+
+#endregion
+
+namespace CS303_05172024;
+
+//student class yaradılmalıdır əlavə olaraq daxilində Id(və ya fin kod), ad, soyad, yaş, email, username, password, telefon nömrəsi, address.
+
+
+public class Students
+{
+    public int id;
+    public string name;
+    public string surname;
+    public int age;
+    public string email;
+    public string pinCode;
+    public string username;
+    public string password;
+    public string phoneNumber;
+    public string address;
+
+
+    //Boş, default və ya parameterless Constructor adlanır. Classdan instance almaq üçün məcbur şəkildə Constructor istifadə edilir.
+    public Students()
+    {
+
+    }
+
+}
