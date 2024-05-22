37cf8e8f6714f439c8c55c92ae08730bdf240ed5
AliyevVilayat <77es4pa@code.edu.az>
2024-05-22 23:53:45 +0400

    Lesson 8 has been uploaded

---
+++ Lesson 8/CS303-05212024/CS303-05212024/Car.cs
@@ -0,0 +1,25 @@
+﻿namespace CS303_21052024;
+
+public class Car
+{
+    //Fields
+    public int id;
+    public string brand;
+    public string model;
+
+
+
+    //Constructor
+    public Car()  //=> default constructor, boş constructor və ya parameterless constructor adlanır.
+    {
+
+    }
+
+    //Method
+    public void ShowInfo()
+    {
+        Console.WriteLine($"{id}'li {brand}'in {model} model avtomobili");
+    }
+
+
+}
---
+++ Lesson 8/CS303-05212024/CS303-05212024/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using CS303_21052024;
+
+//Version 1 instance alinan zaman
+Car car = new Car()
+{
+    id = 1,
+    brand = "Mercedes",
+    model = "CLS 63 AMG"
+};
+
+car.ShowInfo();
+
+//Version 2
+car.id = 2;
+car.brand = "BMW";
+car.model = "X7 M-Line";
+
+car.ShowInfo();
+
+
+
+//Constructor
+//Property
+//Static keyword
+//Non-static
+//Readonly,WriteOnly
+//Inheritence
