2022793c56962e4edad0da1af92dac3ff466348c
AliyevVilayat <77es4pa@code.edu.az>
2024-05-22 23:54:07 +0400

    Lesson 9 has been uploaded

---
+++ Lesson 9/CS303-05222024/CS303-05222024/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using CS303_05222024;
+
+#region Get Instance process
+//Way 1
+Student student = new Student()
+{
+    _name = "Test",
+    _surname = "Testov"
+};
+
+student.DisplayInfo();
+
+//Way 2
+student._name = "Test2";
+student._surname = "Testov2";
+Console.WriteLine(student.GetAge());
+student.DisplayInfo();
+
+
+Student student2 = new Student(1,"ForConstructorName","ForConstructorSurname",33);
+
+student2.DisplayInfo();
+
+Student student3 = new Student("MyTestName","MyTestSurname");
+
+#endregion
+
+Console.WriteLine("-------------------------");
+
+#region Properties
+Student student1 = new Student();
+
+student1.DisplayInfo();
+
+student1._name = "Test";
+Console.WriteLine(student1._name);
+
+
+student1.Age = -25;
+Console.WriteLine(student1.Age);
+
+
+
+int age = student1.GetAge();
+Console.WriteLine("Evellki yas:"+age); //=> Console.WriteLine(student1.GetAge());
+/*
+student1.SetAge(25);
+Console.WriteLine("Hazirdaki yas:" + student1.GetAge()); */
+
+#endregion
---
+++ Lesson 9/CS303-05222024/CS303-05222024/Student.cs
@@ -0,0 +1,84 @@
+﻿namespace CS303_05222024;
+
+//access modifier + static/non-static + ClassName (parameters)
+//Default Constructor or Parameterless Constructor ve ya Bos konstruktor
+//Private olaraq təyin olunan Class member yalnız təyin edildiyi edildiyi Class daxilində əlçatandır.
+//Get və Set methoduna sahib olan Field, Property adlanır.
+
+public class Student
+{
+    public int _id;
+    public string _name;
+    public string _surname;
+    private int _age;
+
+    public const string _pin = "12345AB";  // => const keywordü ilə biz təyin etdiyimiz property və ya filedlara təyin etdiyimiz dəyər assign etməliyik əks təqdirdə compiletime error verəcək
+
+
+    public int Age
+    {
+        get
+        {
+            return _age;
+        }
+
+        set
+        {
+            if (value >= 0)
+            {
+
+
+                _age = value;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+
+
+    public Student()
+    {
+        _name = "Zamin";
+        _surname = "Ismayilov";
+        _age = 24;
+
+    }
+
+    public Student(string name, string surname) : this(0, name, surname, 99)
+    {
+        Console.WriteLine(_age);
+        Console.WriteLine("From A");
+    }
+
+    public Student(int Id, string name, string surname, int age)
+    {
+        Console.WriteLine("From B");
+        _id = Id;
+        _name = name;
+        _surname = surname;
+        _age = age;
+
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Telebenin haqqinda melumat:{_name}  {_surname}");
+
+    }
+
+
+    public int GetAge()
+    {
+
+        return _age;
+    }
+
+    public void SetAge(int age)
+    {
+        _age = age;
+    }/**/
+}
+
+
