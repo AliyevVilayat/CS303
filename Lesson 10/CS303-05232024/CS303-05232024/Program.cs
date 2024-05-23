fa01bdea3b7cdfed78da1a517d0f48100414a79a
AliyevVilayat <77es4pa@code.edu.az>
2024-05-24 00:21:19 +0400

    Lesson 10 has been uploaded

---
+++ Lesson 10/CS303-05232024/CS303-05232024/Program.cs
@@ -0,0 +1,91 @@
+﻿//Logical operators
+//Console.ReadLine()
+
+using CS303_05232024;
+using System.Reflection;
+/* And examples
+Console.Write("Zehmet olmasa natural eded daxil edin:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num>0 && num % 2 == 0)
+{
+    Console.WriteLine("Eded cut ededdir");
+}
+else
+{
+    Console.WriteLine("Eded cut eded deyil");
+}*/
+#region Notes
+
+//Logical operators
+
+//&& - and (və)  verildiyi şərt daxilində hər iki tərəf yəni sol və sağ tərəf TRUE olduğu halda geriyə TRUE dəyər qaytarır
+
+/* && - and
+ * true + true = true +
+ * true + false = false +
+ * false + true = false +
+ * false + false = false +
+*/
+
+// || - or(və ya) verildiyi şərt daxilində 2 tərəfdən heç olmasa biri TRUE olduğu halda TRUE dəyər geriyə qaytarır
+
+/* || - or
+ * true + true = true
+ * true + false = true
+ * false + true = true
+ * false + false = false
+*/
+
+// ! - not(inkar) işləndiyi şərt daxilində əks dəyər qaytarır yəni TRUE dəyərin qarşısında FALSE , FALSE dəyərin qarşısında gələrsə TRUE dəyər qaytarır.
+
+#endregion
+
+#region Logical Operator's Examples
+
+/*
+Console.Write("Zehmet olmasa Muellimin adini qeyd edin:");
+string name = Console.ReadLine();
+
+
+if(name == "Vilayat")
+{
+    Console.WriteLine("Bu gun ders gunudur");
+}
+else if(name == "Togrul")
+{
+    Console.WriteLine("Bu gun lab gunudur");
+}
+else
+{
+    Console.WriteLine("Daxil edilen muellim bu qrupa ders demir");
+}
+
+*/
+#endregion
+
+
+
+/*Teacher teacher = new Teacher("Vilayat","Aliyev");
+Console.WriteLine(teacher.seriaNumber);*/
+/*Teacher teacher = new Teacher("AZE1234568")
+{
+    //Initialize
+
+};
+teacher.Name = "Togrul";
+teacher.Surname = "Huseynli";
+Console.WriteLine($"TeacherName:{teacher.Name}, SeriaNumber:{teacher.seriaNumber}" );
+*/
+
+//Console.WriteLine(Teacher.pin); => static movzusunda danisacayiq.
+
+
+
+Assembly assembly = Assembly.GetExecutingAssembly();
+
+
+
+Type myType = assembly.GetType("CS303_05232024.Teacher");
+myType.Get
+Console.WriteLine(myType.Name);
---
+++ Lesson 10/CS303-05232024/CS303-05232024/Teacher.cs
@@ -0,0 +1,120 @@
+﻿namespace CS303_05232024;
+
+//Readonly => Həm Fieldlarla həm də Propertylərlə işlənə bilər.
+
+//Yalnız və yalnız təyin olunduğu yerdə və constructor daxilində dəyər set oluna bilər.
+//Qalan heç bir halda dəyər set oluna bilməz(Reflection xaric)
+
+// Readonly Field => Readonly keyword-u ilə işlənir.
+
+//Readonly Property => Readonly keyword istifade edilmir.
+
+//Init only Property => Readonly Propertydən fərqi ondadır ki əlavə olaraq initialize olunduğu yerdə də dəyər mənimsətmək mümkündür. Və get methodu ilə yanaşı init methodu qeyd edilir.
+    
+
+//Const Field => Yalnız həmin Field təyin olunduğu yerdə dəyər mənimsədilir, başqa heç bir yerdə dəyər mənimsətmək olmaz.
+// Classın objecti yox, bir başa olaraq adı ilə çağırılır. 
+
+
+//Class members - fields, properties, constructor(s), indexer
+
+
+
+
+//Exception, throw, try-catch, multiple catch
+public class Teacher
+{
+
+    //1. Fields & Properties
+    private int _id;
+    private string _name;
+    private string _surname;
+    public const string pin = "AB12345";
+    public readonly string seriaNumber = "AZE1234567";
+
+    public int Id
+    {
+        get
+        {
+            return _id;
+        }
+
+        set
+        {
+            if (value > 0)
+            {
+                _id = value;
+            }
+            else
+            {
+                throw new Exception("Id menfi deyer qebul ede bilmez");
+            }
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+
+        set
+        {
+            //if(value!="Vilayat" || value)
+            _name = value;
+        }
+    }
+
+    public string Surname
+    {
+        get
+        {
+            return _surname;
+        }
+
+        set
+        {
+            _surname = value;
+        }
+    }
+
+    public string ReadonlyProp { get;  } = "Test";
+    public string InitOnlyProp { get; init; } = "Test";
+
+    //2.Constructors
+    //access modifier + className
+
+    public Teacher()
+    {
+        seriaNumber = "AA12345678";
+        ReadonlyProp = "Test";
+    }
+
+    public Teacher(string seriaNumber)
+    {
+        this.seriaNumber = seriaNumber;
+    }
+
+    public Teacher(string name, string surname)
+    {
+        _name = name;
+        Surname = surname;  
+    } 
+
+    //3.Methods
+    public void ChangeConstVal()
+    {
+        //pin = "Test"; => compile time error
+    }
+
+    public void ChangeReadonlyVal() {
+
+        //seriaNumber = "Test"; => compile time error
+    }
+
+    public void ChangeReadonlyPropVal() {
+
+        //ReadonlyProp = "Test"; => compile time error
+    }
+}
